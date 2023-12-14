729d2e38683faa643dd7cd8f90892e693000a861
florestefano1975 <153757302+florestefano1975@users.noreply.github.com>
2023-12-14 18:31:48 +0100

    Version 1.5

---
+++ CHANGELOG.md
@@ -0,0 +1,18 @@
+## 15.12.2023
+
+Version 2.0 - New features added
+
+- New option: Face shape
+- New option: Facial expression
+- New option: Facial asymmetry
+- New option: Dimples
+- Bug fixes and optimizations
+
+# 14.12.2023
+
+- Updated nationality list (193 options)
+- Bug fixes and optimizations
+
+## 13.12.2023
+
+Version 1.0 - First release
--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 # PORTRAIT MASTER
 # Created by AI Wiz Art (Stefano Flore)
-# Version: 1.05
+# Version: 1.5
+# https://stefanoflore.it
+# https://ai-wiz.art
 
 import os
 
@@ -24,6 +26,14 @@ gender_list = pmReadTxt(os.path.join(script_dir, "lists/gender_list.txt"))
 gender_list.sort()
 gender_list = ['-'] + gender_list
 
+face_shape_list = pmReadTxt(os.path.join(script_dir, "lists/face_shape_list.txt"))
+face_shape_list.sort()
+face_shape_list = ['-'] + face_shape_list
+
+facial_expressions_list = pmReadTxt(os.path.join(script_dir, "lists/face_expression_list.txt"))
+facial_expressions_list.sort()
+facial_expressions_list = ['-'] + facial_expressions_list
+
 nationality_list = pmReadTxt(os.path.join(script_dir, "lists/nationality_list.txt"))
 nationality_list.sort()
 nationality_list = ['-'] + nationality_list
@@ -39,6 +49,7 @@ class PortraitMaster:
 
     @classmethod
     def INPUT_TYPES(s):
+        max_float_value = 1.75
         return {
             "required": {
                 "shot": (shot_list, {
@@ -48,21 +59,11 @@ class PortraitMaster:
                     "default": 0,
                     "step": 0.05,
                     "min": 0,
-                    "max": 1.75,
+                    "max": max_float_value,
                     "display": "slider",
                 }),
                 "gender": (gender_list, {
                     "default": gender_list[0],
-                }),
-                "hair_style": (hair_style_list, {
-                    "default": hair_style_list[0],
-                }),
-                "disheveled": ("FLOAT", {
-                    "default": 0,
-                    "min": 0,
-                    "max": 1.50,
-                    "step": 0.05,
-                    "display": "slider",
                 }),
                 "nationality_1": (nationality_list, {
                     "default": nationality_list[0],
@@ -77,6 +78,43 @@ class PortraitMaster:
                     "step": 0.05,
                     "display": "slider",
                 }),
+                "facial_expression": (facial_expressions_list, {
+                    "default": facial_expressions_list[0],
+                }),
+                "facial_expression_weight": ("FLOAT", {
+                    "default": 0,
+                    "step": 0.05,
+                    "min": 0,
+                    "max": max_float_value,
+                    "display": "slider",
+                }),
+                "face_shape": (face_shape_list, {
+                    "default": face_shape_list[0],
+                }),
+                "face_shape_weight": ("FLOAT", {
+                    "default": 0,
+                    "step": 0.05,
+                    "min": 0,
+                    "max": max_float_value,
+                    "display": "slider",
+                }),
+                "facial_asymmetry": ("FLOAT", {
+                    "default": 0,
+                    "min": 0,
+                    "max": max_float_value,
+                    "step": 0.05,
+                    "display": "slider",
+                }),
+                "hair_style": (hair_style_list, {
+                    "default": hair_style_list[0],
+                }),
+                "disheveled": ("FLOAT", {
+                    "default": 0,
+                    "min": 0,
+                    "max": max_float_value,
+                    "step": 0.05,
+                    "display": "slider",
+                }),
                 "age": ("INT", {
                     "default": 30,
                     "min": 18,
@@ -84,66 +122,73 @@ class PortraitMaster:
                     "step": 1,
                     "display": "slider",
                 }),
-                "freckles": ("FLOAT", {
-                    "default": 0,
-                    "min": 0,
-                    "max": 1.50,
-                    "step": 0.05,
-                    "display": "slider",
-                }),
                 "skin_details": ("FLOAT", {
                     "default": 0,
                     "min": 0,
-                    "max": 1.50,
+                    "max": max_float_value,
                     "step": 0.05,
                     "display": "slider",
                 }),
                 "skin_pores": ("FLOAT", {
                     "default": 0,
                     "min": 0,
-                    "max": 1.50,
+                    "max": max_float_value,
+                    "step": 0.05,
+                    "display": "slider",
+                }),
+                "dimples": ("FLOAT", {
+                    "default": 0,
+                    "min": 0,
+                    "max": max_float_value,
+                    "step": 0.05,
+                    "display": "slider",
+                }),
+                "freckles": ("FLOAT", {
+                    "default": 0,
+                    "min": 0,
+                    "max": max_float_value,
                     "step": 0.05,
                     "display": "slider",
                 }),
                 "moles": ("FLOAT", {
                     "default": 0,
                     "min": 0,
-                    "max": 1.50,
+                    "max": max_float_value,
                     "step": 0.05,
                     "display": "slider",
                 }),
                 "skin_imperfections": ("FLOAT", {
                     "default": 0,
                     "min": 0,
-                    "max": 1.50,
+                    "max": max_float_value,
                     "step": 0.05,
                     "display": "slider",
                 }),
                 "eyes_details": ("FLOAT", {
                     "default": 0,
                     "min": 0,
-                    "max": 1.50,
+                    "max": max_float_value,
                     "step": 0.05,
                     "display": "slider",
                 }),
                 "iris_details": ("FLOAT", {
                     "default": 0,
                     "min": 0,
-                    "max": 1.50,
+                    "max": max_float_value,
                     "step": 0.05,
                     "display": "slider",
                 }),
                 "circular_iris": ("FLOAT", {
                     "default": 0,
                     "min": 0,
-                    "max": 1.50,
+                    "max": max_float_value,
                     "step": 0.05,
                     "display": "slider",
                 }),
                 "circular_pupil": ("FLOAT", {
                     "default": 0,
                     "min": 0,
-                    "max": 1.50,
+                    "max": max_float_value,
                     "step": 0.05,
                     "display": "slider",
                 }),
@@ -168,7 +213,7 @@ class PortraitMaster:
 
     CATEGORY = "AI WizArt"
 
-    def pm(self, shot="-", shot_weight=1, gender="-", nationality_1="-", nationality_2="-", nationality_mix=0.5, age=30, hair_style="-", disheveled=0, freckles=0, skin_pores=0, skin_details=0, moles=0, skin_imperfections=0, eyes_details=1, iris_details=1, circular_iris=1, circular_pupil=1, prompt_additional="", prompt_start="", prompt_end=""):
+    def pm(self, shot="-", shot_weight=1, gender="-", facial_expression="-", facial_expression_weight=0, face_shape="-", face_shape_weight=0, nationality_1="-", nationality_2="-", nationality_mix=0.5, age=30, hair_style="-", disheveled=0, dimples=0, freckles=0, skin_pores=0, skin_details=0, moles=0, skin_imperfections=0, eyes_details=1, iris_details=1, circular_iris=1, circular_pupil=1, facial_asymmetry=0, prompt_additional="", prompt_start="", prompt_end=""):
 
         prompt = []
 
@@ -178,7 +223,8 @@ class PortraitMaster:
             gender = gender + " "
 
         if nationality_1 != '-' and nationality_2 != '-':
-            nationality = f"[{nationality_1}:{nationality_2}:{nationality_mix}] "
+            nationality_mix_diff = 1 - round(nationality_mix, 2)
+            nationality = f"[{nationality_1}:{nationality_2}:{round(nationality_mix, 2)}:{nationality_mix_diff}]"
         elif nationality_1 != '-':
             nationality = nationality_1 + " "
         elif nationality_2 != '-':
@@ -193,6 +239,12 @@ class PortraitMaster:
             prompt.append(f"({shot}:{round(shot_weight, 2)})")
 
         prompt.append(f"{nationality}{gender}{round(age)}-years-old")
+
+        if facial_expression != "-":
+            prompt.append(f"({facial_expression}, {facial_expression} expression:{facial_expression_weight})")
+
+        if face_shape != "-":
+            prompt.append(f"({face_shape} shape face:{face_shape_weight})")
 
         if hair_style != "-":
             prompt.append(f"({hair_style} hairstyle:1.25)")
@@ -212,6 +264,9 @@ class PortraitMaster:
         if skin_imperfections > 0:
             prompt.append(f"(skin imperfections:{round(skin_imperfections, 2)})")
 
+        if dimples > 0:
+            prompt.append(f"(dimples:{round(dimples, 2)})")
+
         if freckles > 0:
             prompt.append(f"(freckles:{round(freckles, 2)})")
 
@@ -230,6 +285,9 @@ class PortraitMaster:
         if circular_pupil > 0:
             prompt.append(f"(circular pupil:{round(circular_pupil, 2)})")
 
+        if facial_asymmetry > 0:
+            prompt.append(f"(facial asymmetry, face asymmetry:{round(facial_asymmetry, 2)})")
+
         if prompt_end != "":
             prompt.append(f"{prompt_end}")
 
@@ -246,5 +304,5 @@ NODE_CLASS_MAPPINGS = {
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "PortraitMaster": "Portrait Master by AI WizArt"
+    "PortraitMaster": "Portrait Master by AI Wiz Art (Stefano Flore)"
 }
